12c98e71776c53f516ef4b1e4411c301faa0a1d8
gshduet <modis2199@gmail.com>
2022-02-18 18:32:07 +0900

    220218 beakjoon

--- baekjoon/220218_03052.py
+++ baekjoon/220218_03052.py
@@ -1,4 +1,4 @@
-'''
+'''https://www.acmicpc.net/problem/3052
 두 자연수 A와 B가 있을 때, A%B는 A를 B로 나눈 나머지 이다. 예를 들어, 7, 14, 27, 38을 3으로 나눈 나머지는 1, 2, 0, 2이다. 
 수 10개를 입력받은 뒤, 이를 42로 나눈 나머지를 구한다. 그 다음 서로 다른 값이 몇 개 있는지 출력하는 프로그램을 작성하시오.
 
@@ -8,18 +8,14 @@
 출력
 첫째 줄에, 42로 나누었을 때, 서로 다른 나머지가 몇 개 있는지 출력한다.
 '''
-from dataclasses import dataclass
+input_list = []
+div_42 = []
 
+for i in range(10):
+    input_list.append(int(input()))
 
-@dataclass
-class Rectangle:
-    width: int
-    height: int
+for i in input_list:
+    div_42.append(i % 42)
 
-    @property
-    def area(self):
-        return self.width * self.height
-
-
-rect = Rectangle(3, 4)
-print(rect.area)
+set_list = set(div_42)
+print(len(set_list))
