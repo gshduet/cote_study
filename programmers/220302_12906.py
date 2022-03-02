27c3ca21ae6fdc2dd7732e77709e96746167f73c
gshduet <modis2199@gmail.com>
2022-03-02 12:53:09 +0900

    220302 study

--- programmers/220302_12906.py
+++ programmers/220302_12906.py
@@ -16,11 +16,12 @@ arr = [4, 4, 4, 3, 3] 이면 [4, 3] 을 return 합니다.
 
 
 def solution(arr):
-    answer = []
+    answer = [arr[0]]
     for i in range(1, len(arr)):
         if arr[i] != arr[i-1]:
             answer.append(arr[i])
     return answer
 
-arr = [1,1,3,3,0,1,1]
+
+arr = [1, 1, 3, 3, 0, 1, 1]
 print(solution(arr))
