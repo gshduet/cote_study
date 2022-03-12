ca41f7bb02e0b99f48b318c6655669e4efbb551c
gshduet <modis2199@gmail.com>
2022-03-12 18:14:06 +0900

    220312 study_time_check_decorator_2

--- programmers/utils.py
+++ programmers/utils.py
@@ -6,10 +6,9 @@ def time_check(solution):
     def wrapper(*args, **kwargs):
         
         start_time = time()
-        ret = solution(*args, **kwargs)
         end_time = time()
 
         print(f'operating time = {end_time - start_time}')
-        return ret
+        return solution(*args, **kwargs)
 
     return wrapper
